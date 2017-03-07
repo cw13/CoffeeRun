fce548482204cccae215d4cab76b2db33a051696
Callum Wilkinson <callumwilkinson@callums-mbp.home>
2017-03-07 22:04:11 +0000

    added displaying and removing orders to coffee run

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -6,20 +6,20 @@
         this.data = {};
     }
 
-    DataStore.prototype.add = function (key, val) {
-      this.data[key] = val;
+    DataStore.prototype.add = function(key, val) {
+        this.data[key] = val;
     }
 
-    DataStore.prototype.get = function (key) {
-      return this.data[key];
+    DataStore.prototype.get = function(key) {
+        return this.data[key];
     }
 
-    DataStore.prototype.getAll = function () {
-      return this.data;
+    DataStore.prototype.getAll = function() {
+        return this.data;
     }
 
-    DataStore.prototype.remove = function (key) {
-      delete this.data[key];
+    DataStore.prototype.remove = function(key) {
+        delete this.data[key];
     }
 
     App.DataStore = DataStore;
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,27 @@
-(function (window) {
-  'use strict';
-  var FORM_SELECTOR = '[data-coffee-order="form"]';
-  var SLIDER_SELECTOR = '[is-slider="yes"]'
-  var App = window.App;
-  var Truck = App.Truck;
-  var DataStore = App.DataStore;
-  var FormHandler = App.FormHandler;
-  var sliderValue = App.sliderValue;
-  var myTruck = new Truck('Serenity', new DataStore());
-  window.myTruck = myTruck;
+(function(window) {
+    'use strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var SLIDER_SELECTOR = '[is-slider="yes"]'
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
 
-  var formHandler = new FormHandler(FORM_SELECTOR);
-  formHandler.addSliderHandler(SLIDER_SELECTOR);
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-  console.log(formHandler);
+    var App = window.App;
+    var Truck = App.Truck;
+    var DataStore = App.DataStore;
+    var FormHandler = App.FormHandler;
+    var sliderValue = App.sliderValue;
+    var CheckList = App.CheckList;
+
+    var myTruck = new Truck('Serenity', new DataStore());
+    window.myTruck = myTruck;
+
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+    var formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSliderHandler(SLIDER_SELECTOR);
+    formHandler.addSubmitHandler(function(data) {
+        myTruck.createOrder(data);
+        checkList.addRow.call(checkList, data);
+    });
+    console.log(formHandler);
 })(window);
